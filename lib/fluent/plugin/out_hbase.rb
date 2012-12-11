141f864bb87d8a53afba99adecb9a006f5777ae3
loveshell <82163261@qq.com>
2012-12-11 16:23:48 +0800

    add time_format
    add time_format

--- lib/fluent/plugin/out_hbase.rb
+++ lib/fluent/plugin/out_hbase.rb
@@ -8,25 +8,13 @@ module Fluent
       require 'massive_record'
     end
 
-    # Format dates with ISO 8606 by default
-    # http://www.w3.org/TR/NOTE-datetime
-    config_param :time_format, :string, :default => '%Y-%m-%dT%H:%M:%S.%L%:z'
-
-    include SetTagKeyMixin
-    config_set_default :include_tag_key, false
-    config_set_default :tag_key, nil
-
-    include SetTimeKeyMixin
-    config_set_default :include_time_key, false
-    config_set_default :time_key, nil
-
     config_param :tag_column_name, :string, :default => nil
     config_param :time_column_name, :string, :default => nil
     config_param :fields_to_columns_mapping, :string
     config_param :hbase_host, :string, :default => 'localhost'
     config_param :hbase_port, :integer, :default => 9090
     config_param :hbase_table, :string
-
+    config_param :time_format, :string, :default => nil
     def configure(conf)
       super
 
@@ -34,6 +22,7 @@ module Fluent
         src_to_dst.split("=>")
       }
       @mapping = Hash[*@fields_to_columns.flatten]
+      @timef = TimeFormatter.new(@time_format, @localtime)
     end
 
     def start
@@ -41,11 +30,10 @@ module Fluent
 
       @conn = MassiveRecord::Wrapper::Connection.new(:host => @hbase_host, :port => @hbase_port)
       @table = MassiveRecord::Wrapper::Table.new(@conn, @hbase_table.intern)
-
       unless @table.exists?
         columns = [@tag_column_name, @time_column_name] + @mapping.values
         column_families = columns.map {|column_family_with_column|
-          column_family, column = column_family_with_column.split(":")
+         column_family, column = column_family_with_column.split(":")
 
           if column.nil?
             raise <<MESSAGE
@@ -66,9 +54,9 @@ MESSAGE
 
     def format(tag, time, record)
       row_values = {}
-
+      time_str = @timef.format(time)
       row_values[@tag_column_name] = tag unless @tag_column_name.nil?
-      row_values[@time_column_name] = time unless @time_column_name.nil?
+      row_values[@time_column_name] = time_str unless @time_column_name.nil?
 
       @fields_to_columns.each {|field,column|
 
@@ -91,21 +79,18 @@ MESSAGE
     def write(chunk)
       chunk.msgpack_each {|row_values|
         event = {}
-
         row_values.each {|column_family_and_column, value|
           column_family, column = column_family_and_column.split(":")
-
           (event[column_family.intern] ||= {}).update({column => value})
         }
-
         row = MassiveRecord::Wrapper::Row.new
-        row.id = SecureRandom.uuid
+        t=Time.now
+        row.id   = t.strftime("%Y%m%d%H%M%S")+"-"+SecureRandom.uuid 
         row.values = event
         row.table = @table
         row.save
       }
     end
-
   end
 
 end
